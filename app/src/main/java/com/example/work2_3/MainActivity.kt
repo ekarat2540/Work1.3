5286b5b53551617b6486470d77e0360865f6e185
Ekarat Sukprasert <59160993@go.buu.ac.th>
2019-08-21 15:48:41 +0700

    Add click to view

--- app/src/main/java/com/example/work2_3/MainActivity.kt
+++ app/src/main/java/com/example/work2_3/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.work2_3
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+    private fun makeColored(view: View){
+        when (view.id){
+            R.id.box_one_textbox -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_textbox -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text_textbox -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text_textbox -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text_textbox -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun setListeners(){
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val clickableViews: List<View> =
+                listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText)
+
+        for(item in clickableViews) {
+            item.setOnClickListener { makeColored(it)}
+        }
+
     }
 }
